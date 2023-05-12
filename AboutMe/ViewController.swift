0e9b0b0082623b4ab7f68cc8162fd16246f6bce2
Zalman Zoteev <sz.zoteev@gmail.com>
2023-05-12 12:46:41 +0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Zalman Zoteev on 12/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
